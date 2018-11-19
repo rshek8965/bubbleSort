52366ec4b4839e4012fc9390f81e5e505eed43f9
rshek8965 <rshek8965@bths.edu>
2018-11-19 11:28:01 -0500

    fixed code

--- src/com/company/bubble.java
+++ src/com/company/bubble.java
@@ -2,13 +2,17 @@ package com.company;
 
 public class bubble {
     public static void bubbleSwap(int[] arr) {
-        for (int i=0; i<arr.length; i++) {
-            if (arr[i] > arr[i+1]) {
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+        for(int k = 0; k < arr.length; k++)
+        {
+            for (int i=0; i<arr.length-1; i++)
+            {
+                if (arr[i] > arr[i+1])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
             }
         }
     }
-
 }
--- src/com/company/runner.java
+++ src/com/company/runner.java
@@ -3,7 +3,7 @@ package com.company;
 public class runner {
 
     public static void main(String[] args) {
-	int[] array = {5,4,3,2,1};
-	bubbleSwap(int[]array);
+	int[] arr = {5,4,3,2,1};
+	bubble.bubbleSwap(arr);
     }
 }
